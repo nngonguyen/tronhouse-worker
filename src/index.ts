3a8b859d8d34838e5b1b9843f4930f06cffc29ab
joehua87 <joehua87@gmail.com>
2021-01-12 10:29:21 +0700

    prototype watch files

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,41 @@
-export function add(a: number, b: number) {
-  return a + b
+import chokidar from 'chokidar'
+
+const root = '/Users/achilles/Workspaces/resources'
+
+// Watch for original files
+chokidar.watch(`${root}/*/original/*`, { ignoreInitial: true }).on('all', (event, path) => {
+  console.log(event, path)
+  const shootId = checkShootStatusByOriginalFile(path)
+  console.log('original file uploaded', { shootId })
+})
+
+// Watch for retouched files
+chokidar.watch(`${root}/*/retouched.*`, { ignoreInitial: true }).on('all', (event, path) => {
+  console.log(event, path)
+  const shootId = checkShootStatusByRetouchedFile(path)
+  console.log('retouched file uploaded', { shootId })
+})
+
+function checkShootStatusByOriginalFile(filePath: string) {
+  const [, , shootId] = filePath.split(/\//g).reverse()
+  return shootId
+}
+
+function checkShootStatusByRetouchedFile(filePath: string) {
+  const [, shootId] = filePath.split(/\//g).reverse()
+  return shootId
+}
+
+function publishRunPreAction(shootId: string) {}
+
+function publishRunPostAction(shootId: string) {}
+
+function consumePreAction(shootId: string) {
+  // Step 1: Make Photoshop script jsx file
+  // Step 2: Execute that file
+}
+
+function consumePostAction(shootId: string) {
+  // Step 1: Make Photoshop script jsx file
+  // Step 2: Execute that file}
 }
