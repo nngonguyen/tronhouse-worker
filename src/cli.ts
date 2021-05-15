6dc86132ca277c0ff011f5d3a52b4ff057dea32e
Joe Hua <joehua87@gmail.com>
2021-05-15 13:58:07 +0700

    correct cli

--- src/cli.ts
+++ src/cli.ts
@@ -6,8 +6,20 @@ import { executePreScript } from './worker'
 
 program
   .version('0.1.0')
-  .command('run-pre-script <shootId>', 'Run pre script')
+  .command('run-pre-script <shootId>')
   .action(async (shootId) => {
+    console.log({ shootId })
+    console.log(`Start shoot ${shootId} data`)
     const shoot = await getShoot(shootId)
-    await executePreScript(shoot)
+    console.log(`Got shoot data, start run pre-script`)
+    try {
+      await executePreScript(shoot)
+      console.log(`Run prescript successfully!`)
+      process.exit(0)
+    } catch (err) {
+      console.error(err)
+      process.exit(1)
+    }
   })
+
+program.parse(process.argv)
